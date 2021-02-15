d1daf9d417cb3faa0499d1b6d8f8b67051cd102c
Alexandr Avakyan <avakjan@mail.ru>
2021-02-15 13:07:13 +0300

    Сorrected First commit

--- public/loginPage.js
+++ public/loginPage.js
@@ -3,9 +3,9 @@
 const userForm = new UserForm();
 
 userForm.loginFormCallback = function(data) {
-  ApiConnector.login(data, result => {result.success ? location.reload() : this.setLoginErrorMessage(result.data)})
+  ApiConnector.login(data, result => {result.success ? location.reload() : this.setLoginErrorMessage(result.error)})
 };
 
 userForm.registerFormCallback = function(data) {
-  ApiConnector.register(data, result => {result.success ? location.reload() : this.setRegisterErrorMessage(result.data)})
+  ApiConnector.register(data, result => {result.success ? location.reload() : this.setRegisterErrorMessage(result.error)})
 };
